d28c380a6fcf3c27840961bafc1189038fd4fd05
tntyousif <yousif02bh@gmail.com>
2024-10-20 10:07:52 +0300

    initial setup for men app

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+require('dotenv').config();
+require('./config/database');
+
+const app = express();
+// Set the port from environment variable or default to 3000
+const port = process.env.PORT ? process.env.PORT : '3000';
+
+//middleware
+
+// Middleware to parse URL-encoded data from forms
+app.use(express.urlencoded({ extended: false }));
+// Middleware for using HTTP verbs such as PUT or DELETE
+app.use(methodOverride('_method'));
+// Morgan for logging HTTP requests
+app.use(morgan('dev'));
+
+//hoisting
+
+app.listen(port, () => {
+  console.log(`The express app is ready on port ${port}!`);
+});
